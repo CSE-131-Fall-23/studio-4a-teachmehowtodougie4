e85513811fa93f74622bfc6bf72c2edcdb6c4f2f
amalia <amalia@amalias-air.dhcp.wustl.edu>
2023-10-05 12:38:31 -0500

    we made a flag for our studio team!

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,48 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		// set variables with right names
+		String shapeType; 
+		int redComponent; 
+		int greenComponent; 
+		int blueComponent; 
+		boolean isFilled; 
+		double x;
+		double y;
+		double halfwidth; 
+		double halfheight; 
+
+		// read through text file in correct order
+		shapeType = in.next();
+		redComponent = in.nextInt();
+		greenComponent = in.nextInt();
+		blueComponent = in.nextInt();
+		isFilled = in.nextBoolean();
+		x = in.nextDouble(); 
+		y = in.nextDouble(); 
+		halfwidth = in.nextDouble(); 
+		halfheight = in.nextDouble(); 
+
+
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		if (shapeType.equals("rectangle")) {
+			if (isFilled == true) {
+				StdDraw.filledRectangle(x, y, halfwidth, halfheight);
+			}
+			else {
+				StdDraw.rectangle(x, y, halfwidth, halfheight);
+			}
+		}
+
+		if (shapeType.equals("ellipse")) {
+			if (isFilled == true) {
+				StdDraw.filledEllipse(x, y, halfwidth, halfheight);
+			}
+			
+			else {
+				StdDraw.ellipse(x, y, halfwidth, halfheight);
+			}
+		}
 	}
 }
